3cb5eec1673f4e93db9f3ee53033f7aba4f39cb2
“brenda-mareco” <brendaamareco@gmail.com>
2024-02-04 14:39:25 -0300

    Added resources folder config

--- Assets/Scripts/DynamicCreation/DynamicObjectCreator.cs
+++ Assets/Scripts/DynamicCreation/DynamicObjectCreator.cs
@@ -5,64 +5,64 @@ using UnityEngine;
 
 public class DynamicObjectCreator : MonoBehaviour
 {
-    [System.Serializable]
-    public struct MapObjectTilePair
-    {
-        public string objectName;
-        public Tile tile;
-    }
+    //[System.Serializable]
+    //public struct MapObjectTilePair
+    //{
+    //    public string objectName;
+    //    public Tile tile;
+    //}
 
-    [SerializeField] MapObjectTilePair[] mapObjectTilePair;
+    //[SerializeField] MapObjectTilePair[] mapObjectTilePair;
     
-    [SerializeField] PrefabData prefabData;
+    //[SerializeField] TileTypeMapperData tileTypeMapperData;
 
-    void Start()
-    {
-        foreach (MapObjectTilePair _object in mapObjectTilePair)
-        {
-            CreatePrefab(_object.tile.transform, _object.objectName);
-        }
-    }
+    //void Start()
+    //{
+    //    foreach (MapObjectTilePair _object in mapObjectTilePair)
+    //    {
+    //        CreatePrefab(_object.tile.transform, _object.objectName);
+    //    }
+    //}
 
-    public void CreatePrefab(Transform parentTransform, string prefabName)
-    {
-        GameObject prefabToInstantiate = GetPrefabByName(prefabName);
-        if(prefabToInstantiate != null)
-        {
-            GameObject prefabGO = Instantiate(prefabToInstantiate);
+    //public void CreatePrefab(Transform parentTransform, string prefabName)
+    //{
+    //    GameObject prefabToInstantiate = GetPrefabByName(prefabName);
+    //    if(prefabToInstantiate != null)
+    //    {
+    //        GameObject prefabGO = Instantiate(prefabToInstantiate);
         
-            Vector3 oldPos = prefabGO.transform.position;
-            Vector3 oldScale = prefabGO.transform.localScale;
-            Quaternion oldRotation = prefabGO.transform.rotation;
+    //        Vector3 oldPos = prefabGO.transform.position;
+    //        Vector3 oldScale = prefabGO.transform.localScale;
+    //        Quaternion oldRotation = prefabGO.transform.rotation;
         
-            prefabGO.transform.parent = parentTransform;
-            prefabGO.transform.localPosition = oldPos;
-            prefabGO.transform.rotation = oldRotation;
+    //        prefabGO.transform.parent = parentTransform;
+    //        prefabGO.transform.localPosition = oldPos;
+    //        prefabGO.transform.rotation = oldRotation;
 
-            Vector3 inverseScale = new Vector3(
-                        1.0f / parentTransform.localScale.x,
-                        1.0f / parentTransform.localScale.y,
-                        1.0f / parentTransform.localScale.z
-                        );
+    //        Vector3 inverseScale = new Vector3(
+    //                    1.0f / parentTransform.localScale.x,
+    //                    1.0f / parentTransform.localScale.y,
+    //                    1.0f / parentTransform.localScale.z
+    //                    );
             
 
-            prefabGO.transform.localScale = new Vector3(
-                        oldScale.x * inverseScale.x,
-                        oldScale.y * inverseScale.y,
-                        oldScale.z * inverseScale.z
-                    );
-        }
-    }
+    //        prefabGO.transform.localScale = new Vector3(
+    //                    oldScale.x * inverseScale.x,
+    //                    oldScale.y * inverseScale.y,
+    //                    oldScale.z * inverseScale.z
+    //                );
+    //    }
+    //}
 
-    GameObject GetPrefabByName(string objectName)
-    {
-        foreach (var pair in prefabData.objectPrefabPairs)
-        {
-            if (pair.objectName == objectName)
-            {
-                return pair.prefab;
-            }
-        }
-        return null;
-    }
+    //GameObject GetPrefabByName(string objectName)
+    //{
+    //    //foreach (var pair in tileTypeMapperData.objectPrefabPairs)
+    //    //{
+    //    //    if (pair.objectName == objectName)
+    //    //    {
+    //    //        return pair.prefab;
+    //    //    }
+    //    //}
+    //    return null;
+    //}
 }
