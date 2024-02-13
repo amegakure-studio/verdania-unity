c7e6cb676649d570833a3abe9177a423dc45f794
“brenda-mareco” <brendaamareco@gmail.com>
2024-02-13 19:24:26 -0300

    Updated signup controller
    Co-authored-by: Cristian Fleita <cristianagustinfleita@gmail.com>

---
+++ Assets/Scripts/Controllers/SignUpController.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+
+public class SignUpController : MonoBehaviour
+{
+    [SerializeField] CharacterMap[] characterMap;
+
+    private TextField usernameTxt;
+    private TextField passwordTxt;
+    private EnumField characterDropdown;
+    private Button signupBtn;
+
+    void Start()
+    {
+        VisualElement root = FindObjectOfType<UIDocument>().rootVisualElement;
+
+        usernameTxt = root.Q<TextField>("Username");
+        passwordTxt = root.Q<TextField>("Password");
+        
+        characterDropdown = root.Q<EnumField>("Character");
+        characterDropdown.RegisterValueChangedCallback<Enum>(CharacterChanged);
+
+        signupBtn = root.Q<Button>("SignupBtn");
+        signupBtn.clicked += SignupBtn_clicked;
+    }
+
+    private void CharacterChanged(ChangeEvent<Enum> evt)
+    {
+        SkinType skintype = (SkinType)characterDropdown.value;
+
+        characterMap.ToList().ForEach(cMap => cMap.characterGo.SetActive(false));
+        characterMap.ToList().Find(cMap => cMap.skinType == skintype).characterGo.SetActive(true);
+    }
+
+    private void SignupBtn_clicked()
+    {
+        try
+        {
+            SessionCreator sessionCreator = UnityUtils.FindOrCreateComponent<SessionCreator>();
+            try
+            {
+                string username = usernameTxt.text;
+                string password = passwordTxt.text;
+
+                sessionCreator.CreateNewPlayer(username, password, (SkinType)characterDropdown.value);
+                StartCoroutine(LoadFarmCorroutine(sessionCreator, username, password));
+            }
+            catch (Exception e) { Debug.LogError("Error in dojo TX" + e); }
+
+        }
+        catch (Exception e) { Debug.LogError("Couldn't signup! " + e); }
+        
+    }
+
+    IEnumerator LoadFarmCorroutine(SessionCreator sessionCreator, string username, string password)
+    {
+        Session session = null;
+
+        while (session == null)
+        {
+            session = sessionCreator.GetSessionFromExistingPlayer(username, password);
+
+            if (session == null)
+                yield return new WaitForSeconds(1f);
+        }
+
+        SceneLoader sceneLoader = UnityUtils.FindOrCreateComponent<SceneLoader>();
+        sceneLoader.LoadNextScene();
+    }
+
+
+
+   [Serializable]
+    private struct CharacterMap
+    {
+        public GameObject characterGo;
+        public SkinType skinType;
+    }
+}
